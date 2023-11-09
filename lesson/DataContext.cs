6f05363a42a37107a5b69218a15f397b63e3e67e
hitmachut <hitmachut@students.local>
2023-11-09 16:03:20 +0200

    first commit

---
+++ lesson/DataContext.cs
@@ -0,0 +1,25 @@
+﻿namespace lesson
+{
+    public class DataContext
+    {
+       
+        public List<Event> events { get; set; }
+        public static int Count = 0;
+        public DataContext()
+        {
+
+            
+            events = new List<Event>();
+            events.Add(new Event() { Id= Count++});
+            //events.Add(new Event
+            //{
+            //    Id = count++,
+            //    Title = value.Title,
+            //    Start = new DateTime(),
+            //    End = value.End
+            //});
+        }
+
+       
+    }
+}
