76aff202bcd4507ccae5741e29c78829134b485d
estihool <estihool3767@gmail.com>
2023-11-22 17:38:14 +0200

    API_Event_addFake

--- lesson/Controllers/EventsController.cs
+++ lesson/Controllers/EventsController.cs
@@ -8,33 +8,56 @@ namespace lesson.Controllers
     [ApiController]
     public class EventsController : ControllerBase
     {
-       private DataContext d;
-        public EventsController(DataContext da)
+       //private DataContext d;
+        //public EventsController(DataContext da)
+        //{
+        //    d = da;
+        //}
+
+        private IDataContext _dataContext;
+
+        public EventsController(IDataContext context)
         {
-            d = da;
+            _dataContext = context;
         }
-       //static int count=0;    
+
+
+
+
+
+
+        //static int count=0;    
         //static List<Event> events=new List<Event> { new Event { Id=count++,Title="First Event",Start= new DateTime(2023,09,07), End = new DateTime(2024, 09, 07) } };
         // GET: api/<EventsController>
         [HttpGet]
-        public IEnumerable<Event> Get()
+        //public IEnumerable<Event> Get()
+        //{
+        //    return _dataContext.events;
+        //}
+        public IActionResult Get()
         {
-            return d.events;
+            return Ok(_dataContext.Events);
         }
 
-        // GET api/<EventsController>/5
-        //[HttpGet("{id}")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        // POST api/<EventsController>
-        [HttpPost]
-        public void Post([FromBody] Event value)
+        //GET api/<EventsController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
         {
-            d.events.Add(new Event { Id =value.Id, Title = value.Title, Start = new DateTime(), End = value.End });
-           return;
+            var ev1= _dataContext.Events.Find(e=>e.Id==id);
+            if (ev1 == null)
+            {
+                return NotFound();
+            }
+            return Ok(ev1);
+        }
+
+        //POST api/<EventsController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Event value)
+        {
+            _dataContext.Events.Add(new Event { Id =value.Id, Title = value.Title, Start = new DateTime(), End = value.End });
+
+            return Ok(value);
 
         }
 
@@ -42,7 +65,7 @@ namespace lesson.Controllers
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] Event value)
         {
-            var eve = d.events.Find(e => e.Id == id);
+            var eve = _dataContext.Events.Find(e => e.Id == id);
             eve.Title=value.Title;
             eve.Start=value.Start;
             eve.End=value.End;
@@ -50,11 +73,15 @@ namespace lesson.Controllers
 
         // DELETE api/<EventsController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
-            var eve = d.events.Find(e => e.Id == id);
-            d.events.Remove(eve);
-             
+            var eve = _dataContext.Events.Find(e => e.Id == id);
+            if (eve == null)
+            {
+                return NotFound();
+            }
+            _dataContext.Events.Remove(eve);
+            return Ok(id);
         }
     }
 }
--- lesson/DataContext.cs
+++ lesson/DataContext.cs
@@ -1,25 +1,42 @@
-﻿namespace lesson
+﻿using CsvHelper;
+using System.Formats.Asn1;
+using System.Globalization;
+
+
+namespace lesson
 {
-    public class DataContext
+    public class DataContext:IDataContext
     {
+        public List<Event> Events { get; set; }
+
+        //public List<Event> events { get; set; }
+        //public static int Count = 0;
+        //public DataContext()
+        //{
+
+
+        //    events = new List<Event>();
+        //    events.Add(new Event() { Id= Count++});
+        //    //events.Add(new Event
+        //    //{
+        //    //    Id = count++,
+        //    //    Title = value.Title,
+        //    //    Start = new DateTime(),
+        //    //    End = value.End
+        //    //});
+        //}
        
-        public List<Event> events { get; set; }
-        public static int Count = 0;
         public DataContext()
         {
-
             
-            events = new List<Event>();
-            events.Add(new Event() { Id= Count++});
-            //events.Add(new Event
-            //{
-            //    Id = count++,
-            //    Title = value.Title,
-            //    Start = new DateTime(),
-            //    End = value.End
-            //});
+            using (var reader = new StreamReader("data.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                Events = csv.GetRecords<Event>().ToList();
+            }
         }
 
-       
+
+
     }
 }
